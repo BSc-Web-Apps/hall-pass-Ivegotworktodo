8996ec9300947b96a5b96952b85b93b0c9e400ec
Joshua Blake <115797116+Ivegotworktodo@users.noreply.github.com>
2025-03-27 16:23:19 +0000

    Made Checkbox bigger and add more Tasks

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,67 @@
 import * as React from "react";
 import { View, Text } from "react-native";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Checkbox } from "~/components/ui/checkbox";
 
+interface TaskProps {
+  title: string;
+  category: string;
+  isChecked: boolean;
+}
+
+function Task({ title, category, isChecked }: TaskProps) {
+  const [checked, setChecked] = React.useState(isChecked);
+  return (
+    <View className="flex flex-row h-20 w-full border-2 border-cyan-400">
+      <View className="flex w-24 h-full border-2 border-red-600 justify-center items-center">
+        <Checkbox
+          className="border-pink-600"
+          checked={checked}
+          onCheckedChange={setChecked}
+          style={{ transform: [{ scale: 2 }] }}
+        />
+      </View>
+      <View className="flex flex-1 h-full border-2">
+        <Text className=" text-gray-300 text-xl">{title}</Text>
+        <Text className="text-foreground-transparent text-xl">{category}</Text>
+      </View>
+    </View>
+  );
+}
+
 export default function HomeScreen() {
-  const [checked, setChecked] = React.useState(false);
+  const tasks = [
+    {
+      id: 1,
+      title: "Read Chapter 9",
+      category: "English Literature",
+      isChecked: false,
+    },
+    {
+      id: 2,
+      title: "Case Study Essay",
+      category: "Geography",
+      isChecked: false,
+    },
+    {
+      id: 3,
+      title: "Book Essay",
+      category: "English Literature",
+      isChecked: false,
+    },
+    {
+      id: 4,
+      title: "Quadratics Worksheet",
+      category: "Maths",
+      isChecked: false,
+    },
+    {
+      id: 5,
+      title: "Draw Design",
+      category: "Design & Technology",
+      isChecked: false,
+    },
+  ];
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-background">
       <Card className="w-full max-w-sm p-6 rounded-2xl">
@@ -20,28 +71,15 @@ export default function HomeScreen() {
         </CardHeader>
 
         <CardContent>
-          <View className="flex flex-row h-20 w-full border-2 border-cyan-400">
-            <View className="flex w-24 h-full border-2">
-              <Checkbox
-                className="border-pink-600"
-                checked={checked}
-                onCheckedChange={setChecked}
-              />
-            </View>
-            <View className="flex flex-1 h-full border-2">
-              <Text className=" text-gray-300 text-xl">Feed the cow</Text>
-              <Text className="text-foreground-transparent text-xl">
-                Feed the cow
-              </Text>
-            </View>
-          </View>
+          {tasks.map((task) => (
+            <Task
+              key={task.id}
+              title={task.title}
+              category={task.category}
+              isChecked={task.isChecked}
+            />
+          ))}
         </CardContent>
-
-        <CardFooter className="flex-col gap-3 pb-6">
-          <View className="flex-row items-center overflow-hidden">
-            <Text>Update me with your app code</Text>
-          </View>
-        </CardFooter>
       </Card>
     </View>
   );
